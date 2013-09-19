bbc5841654f26d81ac1ec552b0b8a49e9dad5712
Vlad Niculae <vlad@vene.ro>
2013-09-19 13:19:44 +0200

    Use lemmas when available

--- scripts/dep_seeds_properties.py
+++ scripts/dep_seeds_properties.py
@@ -5,6 +5,9 @@ from compattern.dependency import match
 from compattern.dependency.seed_patterns import patterns
 
 
+def _lemma_or_form(tok):
+    return tok.form.lower() if tok.lemma == '_' else tok.lemma.lower()
+
 if __name__ == '__main__':
     from compattern.dependency.conll import read
     from compattern.resources.therex import shared_category, poetic_properties
@@ -19,10 +22,10 @@ if __name__ == '__main__':
             print(" ".join(tok.form for tok in sent))
             for m in matches:
                 pprint(m)
-                V_head = m['V'].form.lower()
+                V_head = _lemma_or_form(m['V'])
                 print(poetic_properties(V_head))
                 if 'T' in m:
-                    T_head = m['T'].form.lower()
+                    T_head = _lemma_or_form(m['T'])
                     print("{} x {}".format(T_head, V_head))
                     print(shared_category(T_head, V_head))
     print(n_matches)
