56093bb7189ee511493be514a350e8eed45862fa
Vlad Niculae <vlad@vene.ro>
2013-09-19 19:55:42 +0200

    Add more AS patterns

--- compattern/dependency/seed_patterns.py
+++ compattern/dependency/seed_patterns.py
@@ -25,19 +25,46 @@ like_tr = (None, lambda pos: pos.startswith('VB'), None, Slot(label="_E"),
                 ]),
           (None, None, 'SUB', Slot(label="T", required=False), [])])
 
-as_ = (None, lambda pos: pos.startswith('VB'), None,
-       Slot(label="E", required=True),
-       [
-           (None, None, 'SUB', Slot(label="T", required=False), []),
-           (None, None, 'PRD', Slot(label="P", required=True),
-            [
-                ('as', 'RB', 'AMOD', Slot(label="_C", required=True), []),
-                ('as', 'IN', 'AMOD', Slot(label="C", required=True),
-                 [
-                     (None, None, None, Slot(label='V', required=True), [])
-                 ])
-            ])
-       ])
+as_1 = (None, lambda pos: pos.startswith('VB'), None,
+        Slot(label="E", required=True),
+        [
+            (None, None, 'SUB', Slot(label="T", required=False), []),
+            (None, None, 'PRD', Slot(label="P", required=True),
+             [
+                 ('as', 'RB', 'AMOD', Slot(label="_C", required=True), []),
+                 ('as', 'IN', 'AMOD', Slot(label="C", required=True),
+                  [
+                      (None, None, None, Slot(label='V', required=True), [])
+                  ])
+             ])
+        ])
 
-patterns = [like, like_tr, as_]
+as_2 = (None, lambda pos: pos.startswith('VB'), None,
+        Slot(label="E", required=True),
+        [
+            (None, None, 'SUB', Slot(label="T", required=False), []),
+            ('as', 'IN', 'VMOD', Slot(label="_C", required=True),
+             [
+                 (None, None, None, Slot(label='P', required=True), [])
+             ]),
+            ('as', 'IN', 'VMOD', Slot(label="C", required=True),
+             [
+                 (None, None, None, Slot(label='V', required=True), [])
+             ])
+        ])
 
+as_3 = (None, lambda pos: pos.startswith('VB'), None,
+        Slot(label="E", required=True),
+        [
+            (None, None, 'SUB', Slot(label="T", required=False), []),
+            ('as', 'RB', 'VMOD', Slot(label="_C", required=True),
+             [
+                 (None, None, 'AMOD', Slot(label="P", required=True), []),
+                 ('as', 'IN', 'AMOD', Slot(label="C", required=True),
+                  [
+                      (None, None, None, Slot(label="V", required=True), [])
+                  ])
+             ])
+        ])
+
+patterns = [like, like_tr, as_1, as_2, as_3]
