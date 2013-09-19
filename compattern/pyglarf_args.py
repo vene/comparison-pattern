52e63a9e2449abdcc9ad1b94158c2d78cc299c00
Vlad Niculae <vlad@vene.ro>
2013-09-19 16:10:55 +0200

    Add pyglarf-based extractor

---
+++ compattern/pyglarf_args.py
@@ -0,0 +1,81 @@
+from pyglarf import GlarfTree
+
+def _safe_filter_comparator(node_or_string):
+    """Returns True when passed a comparison trigger node, False elsewhere"""
+    return (hasattr(node_or_string, 'node') and node_or_string.node == 'IN' and
+            node_or_string[0] == 'like')
+
+def _safe_filter_as_unary(arg):
+    anchor = arg[arg.leaf_treeposition(0)[:-1]]
+    return hasattr(anchor, 'node') and anchor.node == 'IN' and anchor[0] == 'as'
+
+
+def _safe_head(node):
+    if (hasattr(node, 'head')):
+        head = node.head()
+        if head:
+            return head[0]
+
+
+def _get_sbj(rel):
+    for role_a, role_b, _, arg_tree in rel.args.values():
+        if (any('SBJ' in role for role in role_a) or 'SBJ' in role_b) and (
+                arg_tree is not None):
+            return arg_tree
+
+
+def find_comparison_nodes(tree):
+    comparison_nodes = []
+    for rel in tree.rels():
+        for _, _, _, arg_trees in rel.args.values():
+            for arg_tree in arg_trees:
+                if _safe_filter_comparator(_safe_head(arg_tree[0])):
+                    comparison_nodes.append((rel, arg_tree))
+                if _safe_filter_as_unary(arg_tree):
+                    comparison_nodes.append((rel, arg_tree))
+        for _, _, arg_tree in rel.advs.values():
+            if _safe_filter_comparator(_safe_head(arg_tree)):
+                comparison_nodes.append((rel, arg_tree))
+            if _safe_filter_as_unary(arg_tree):
+                comparison_nodes.append((rel, arg_tree))
+            #for pos in arg_tree.treepositions():
+                #    if _safe_filter_comparator(arg_tree[pos]):
+                #        comparison_nodes.append((rel, arg_tree))
+    return comparison_nodes
+
+
+def get_args(rel, comparison_arg):
+    try:
+        sbj = _get_sbj(rel)[0].most_specific_head()
+        if len(comparison_arg) > 1:
+            veh = comparison_arg[1].most_specific_head()
+        else:
+            veh = comparison_arg[0][1].most_specific_head()
+        if comparison_arg.node == 'ADJP':
+            gnd = comparison_arg.head()[0].head()
+            veh = comparison_arg[1, 0, 1, 0, 1, 0].head()
+        elif comparison_arg[0].node == 'ADJP':
+            gnd = comparison_arg.most_specific_head()
+            veh = comparison_arg[0, 1, 0, 1, 0, 1, 0].head()
+        else:
+            gnd = None  # we accept partial matches that don't find the gnd
+    except:
+        sbj = GlarfTree('', [])
+        veh = GlarfTree('', [])
+        gnd = None
+    return {
+        'T': sbj[0].print_flat(False, False, False, False) if sbj else "",
+        'E': rel.head,
+        'C': comparison_arg.leaves()[0],
+        'V': veh.print_flat(False, False, False, False) if veh else "",
+        'P': gnd.print_flat(False, False, False, False) if gnd else "",
+    }
+
+def wn_categ(args):
+    from nltk import wordnet
+    from nltk.stem import WordNetLemmatizer
+    lmtzr = WordNetLemmatizer()
+    sbj = lmtzr.lemmatize(args['T'].lower())
+    veh = lmtzr.lemmatize(args['V'].lower())
+    return (wordnet.wordnet.lemmas(sbj),
+            wordnet.wordnet.lemmas(veh))
---
+++ scripts/minibnc.py
@@ -0,0 +1,80 @@
+import sys
+
+PATH = '/Users/vene/bnc/similes/{}/'.format(sys.argv[1])
+
+from glob import glob
+from codecs import open
+from yaml import dump, load
+from pyglarf import GlarfTree
+from compattern.pyglarf_args import get_args, find_comparison_nodes
+from compattern.dependency import match
+from compattern.dependency.seed_patterns import patterns
+
+from compattern.dependency.conll import read
+dep_parse = read(open('../data/bnc_{}_lemma.conll'.format(sys.argv[1])),
+                 return_tree=True)
+examples = []
+for f in glob(PATH + '*.yaml'):
+    f = open(f, encoding='utf-8')
+    examples.extend(load(f))
+
+sents, ctxs, gfs, gts = zip(*examples)
+f = None
+only_glarf = open("bnc_similes/{}/only_glarf.txt".format(sys.argv[1]), "w",
+                  encoding="utf-8")
+only_dep = open("bnc_similes/{}/only_dep.txt".format(sys.argv[1]), "w",
+                encoding="utf-8")
+matches = 0
+dep_matches = 0
+for ii, (sent, ctx, gf, gt, dep) in enumerate(zip(sents, ctxs, gfs, gts,
+                                                  dep_parse)):
+    dep = dep[1]
+    if ii % 20 == 0:
+        print '.'
+        if f:
+            f.close()
+        f = open('bnc_similes/{}/{:03d}.txt'.format(sys.argv[1], ii / 20), 'w',
+                 encoding='utf-8')
+    try:
+        tree = GlarfTree.glarf_parse(gf, gt)
+        args = [get_args(*node) for node in find_comparison_nodes(tree)]
+        args = [arg_dict for arg_dict in args
+                if arg_dict['C'].lower() == sys.argv[1]]
+    except ValueError:
+        args = []
+        continue
+
+    dep_args = [m for pat in patterns for m in match(dep, pat)
+                if m['C'].form.lower() == sys.argv[1]]
+
+    if args:
+        matches += 1
+    if dep_args:
+        dep_matches += 1
+
+    print_to = [f]
+    if args and not dep_args:
+        print_to.append(only_glarf)
+    elif dep_args and not args:
+        print_to.append(only_dep)
+
+    for dest in print_to:
+        print >> dest, sent
+        print >> dest
+        print >> dest, "Glarf:"
+        for arg in args:
+            print >> dest, "T: {T}\nE: {E}\nP: {P}\nC: {C}\nV: {V}\n".format(
+                **arg)
+        print >> dest, "\nDep:"
+        for arg in dep_args:
+            arg = {key: unicode(val.form, errors="ignore")
+                   for key, val in arg.iteritems()}
+            for K in 'TEPCV':
+                if K not in arg:
+                    arg[K] = '--'
+            print >> dest, "T: {T}\nE: {E}\nP: {P}\nC: {C}\nV: {V}\n".format(
+                **arg)
+        print >> dest, "==="
+
+print matches
+print dep_matches
