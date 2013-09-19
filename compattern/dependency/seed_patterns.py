36ab120eec4ace5f7ddb52122dae17182cce38c9
Vlad Niculae <vlad@vene.ro>
2013-09-19 16:12:59 +0200

    Remove unused code

--- compattern/dependency/seed_patterns.py
+++ compattern/dependency/seed_patterns.py
@@ -1,6 +1,4 @@
-from __future__ import print_function
-
-from compattern.dependency import Slot, match
+from compattern.dependency import Slot
 
 like = (None, lambda pos: pos.startswith('VB'), None,
         Slot(label="E", required=True),
@@ -12,6 +10,7 @@ like = (None, lambda pos: pos.startswith('VB'), None,
             (None, None, 'SUB', Slot(label="T", required=False), []),
             (None, None, 'PRD', Slot(label="P", required=False), [])
         ])
+
 like_tr = (None, lambda pos: pos.startswith('VB'), None, Slot(label="_E"),
            [
                (None, lambda pos: pos.startswith('VB'), 'VC',
@@ -25,6 +24,7 @@ like_tr = (None, lambda pos: pos.startswith('VB'), None, Slot(label="_E"),
                     (None, None, 'PRD', Slot(label="P", required=False), [])
                 ]),
           (None, None, 'SUB', Slot(label="T", required=False), [])])
+
 as_ = (None, lambda pos: pos.startswith('VB'), None,
        Slot(label="E", required=True),
        [
@@ -41,29 +41,3 @@ as_ = (None, lambda pos: pos.startswith('VB'), None,
 
 patterns = [like, like_tr, as_]
 
-if __name__ == '__main__':
-    from compattern.dependency.conll import read
-    from compattern.resources.therex import shared_category, poetic_properties
-
-    from pprint import pprint
-    with open("/Users/vene/Dropbox/similes_2013/data/"
-              "hanks_turboparsed/hanks_turboparsed.txt") as f:
-        vuamc = read(f, return_tree=True)
-                     #"/hanks_rest.txt")
-    #vuamc = open_conll("/Users/vene/corpora/2541/vuamc.conll")
-    #vuamc = open_conll("/Users/vene/code/conll-depchunks/property.txt")
-    n_matches = 0
-    for sent, root in vuamc:
-        matches = [m for pat in patterns for m in match(root, pat)]
-        if matches:
-            n_matches += len(matches)
-            print(" ".join(tok.form for tok in sent))
-            for m in matches:
-                pprint(m)
-                V_head = m['V'].form.lower()
-                print(poetic_properties(V_head))
-                if 'T' in m:
-                    T_head = m['T'].form.lower()
-                    print("{} x {}".format(T_head, V_head))
-                    print(shared_category(T_head, V_head))
-    #print(n_matches)
